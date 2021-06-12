e8fcf341e7038d08f7d20fa3cb704bdc2522938f
anand228 <anandsrivastav228@gmail.com>
2021-06-12 22:32:00 +0530

    uploade image and store in realtime database

--- app/src/main/java/com/example/firebasestorageanddatabase/MainActivity.java
+++ app/src/main/java/com/example/firebasestorageanddatabase/MainActivity.java
@@ -1,14 +1,155 @@
 package com.example.firebasestorageanddatabase;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.InputStream;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText name , rollNo , course , contact ;
+    private Button signup , browse ;
+    Bitmap bitmap;
+    Uri filepath;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = findViewById(R.id.imageView);
+        signup = findViewById(R.id.buttonsingup);
+        browse = findViewById(R.id.buttonbrowse);
+        //manage permissions
+        browse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dexter.withActivity(MainActivity.this)
+                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse response) {
+                                // lets the user browse files
+                                Intent intent = new Intent(Intent.ACTION_PICK);
+                                intent.setType("image/*");
+                                startActivityForResult(Intent.createChooser(intent,"select image"), 1);
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                                //on re-opening the app this will make it ask for permissions again
+                                token.continuePermissionRequest();
+                            }
+                        }).check();
+            }
+        });
+
+        signup.setOnClickListener(view -> {
+            uploadToFirebase();
+        });
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable  Intent data) {
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            filepath = data.getData();
+            try {
+                //displays selected image in image view
+                InputStream inputstream = getContentResolver().openInputStream(filepath);
+                bitmap = BitmapFactory.decodeStream(inputstream);
+                img.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void uploadToFirebase() {
+
+        ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setTitle("file uploader");
+        dialog.show();
+
+        name = findViewById(R.id.Name);
+        contact = findViewById(R.id.contactno);
+        course = findViewById(R.id.course);
+        rollNo = findViewById(R.id.Rollno);
+
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference uploader = storage.getReference("image1"+ new Random().nextInt(50));
+
+        uploader.putFile(filepath)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        uploader.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                dialog.dismiss();
+                                FirebaseDatabase db = FirebaseDatabase.getInstance();
+                                DatabaseReference root = db.getReference("users");
+
+                                dataholder obj = new dataholder(name.getText().toString(), contact.getText().toString(), course.getText().toString(), uri.toString());
+                                root.child(rollNo.getText().toString()).setValue(obj);
+
+                                name.setText("");
+                                contact.setText("");
+                                course.setText("");
+                                rollNo.setText("");
+                                img.setImageResource(R.drawable.ic_launcher_background);
+                                Toast.makeText(getApplicationContext(),"uploaded", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        float per = (100*snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
+                        dialog.setMessage("uploaded : " + (int)per + "%");
+                    }
+                });
+    }
+
+
 }
